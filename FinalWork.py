c1bda9316bffcf65e1100af7d9b533a75289d6f4
JefersonPompermaier <jefepompermaier@gmail.com>
2023-02-26 20:49:55 -0300

    Primeira função funcional do programa

--- FinalWork.py
+++ FinalWork.py
@@ -1,1 +1,33 @@
-print("AGENDA")
+#inicializando agenda como dicionario para armazenar os dados
+agenda = {}
+
+#Funcoes chamadas pelo programa
+def incluir_compromisso(data, hora, descricao)
+    if data not in agenda:
+        agenda[data]={}
+    agenda[data][hora]=descricao
+
+#Inicializacao do programa, selecionando funcoes por numerais
+
+while True:
+    print("\nAgenda de Compromissos")
+    print("1. Incluir Compromisso")
+    print("2. Consultar Compromisso")
+    print("3. Alterar Compromisso")
+    print("4. Excluir compromisso")
+    print("5. Listar Compromissos")
+    print("6. Sair da Agenda")
+    escolha = int(input("Escolha uma opção: "))
+
+    if escolha == 1:
+        data = input("Informe a data (dd/mm/aaaa): ")
+        hora = input("Informe a hora (hh:mm): ")
+        descricao = input("Informe a descrição: ")
+
+#concatenando os dados em uma funcao
+        incluir_compromisso(data, hora, descricao)
+    
+    if escolha == 5:
+        break
+    else:
+        print("Opção inválida.")
