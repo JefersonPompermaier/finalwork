80f4bd8c58a5222c60005c352188d7a0ef072d2d
JefersonPompermaier <jefepompermaier@gmail.com>
2023-02-27 01:06:04 -0300

    Atualização do código, novas funções adicionadas

--- FinalWork.py
+++ FinalWork.py
@@ -1,13 +1,56 @@
-#inicializando agenda como dicionario para armazenar os dados
+# inicializando agenda como dicionario para armazenar os dados
 agenda = {}
 
-#Funcoes chamadas pelo programa
-def incluir_compromisso(data, hora, descricao)
-    if data not in agenda:
-        agenda[data]={}
-    agenda[data][hora]=descricao
+# Funcoes chamadas pelo programa
 
-#Inicializacao do programa, selecionando funcoes por numerais
+# funcao para inserir compromisso na data informada
+
+
+def inserir_compromisso(data, hora, descricao):
+    if data not in agenda:
+        agenda[data] = {}
+    agenda[data][hora] = descricao
+
+# funcao para verificar se ja existem compromissos salvos
+
+
+def incluir_novo_compromisso(data, hora, descricao):
+
+    if data in agenda and hora in agenda[data]:
+        print("Ja existem compromissos neste horário. Tente verificar e alterar o compromisso.")
+        return
+    else:
+        inserir_compromisso(data, hora, descricao)
+
+# funcao para encontrar compromissos por data e hora ou somente por data
+
+
+def consult_compromisso(data, hora=None):
+    if data not in agenda:
+        print("Nenhum compromisso encontrado nesta data.")
+        return
+    if hora:
+        if hora in agenda[data]:
+            print(agenda[data][hora])
+        else:
+            print("Nenhum compromisso encontrado nesta hora.")
+
+# se a leitura de hora for igual a 0 o programa retorna todos os compromissos da data especificada
+    if hora is not None and len(hora) == 0:
+        print(agenda[data])
+
+# funcao para alterar compromissos
+
+
+def alt_compromisso(data, hora, descricao):
+    if data not in agenda:
+        print("Nenhum compromisso nesta data e hora, será incluído um novo compromisso.\n")
+        inserir_compromisso(data, hora, descricao)
+    else:
+        inserir_compromisso(data, hora, descricao)
+
+
+# Inicializacao do programa, selecionando funcoes por numerais
 
 while True:
     print("\nAgenda de Compromissos")
@@ -17,17 +60,34 @@ while True:
     print("4. Excluir compromisso")
     print("5. Listar Compromissos")
     print("6. Sair da Agenda")
-    escolha = int(input("Escolha uma opção: "))
 
-    if escolha == 1:
+# inseri a variavel como string para caso o usuario digite de forma incorreta o programa de erro
+    escolha = str(input("Escolha uma opção: "))
+
+    if escolha == '1':
         data = input("Informe a data (dd/mm/aaaa): ")
         hora = input("Informe a hora (hh:mm): ")
         descricao = input("Informe a descrição: ")
+# concatenando os dados em uma funcao
+        incluir_novo_compromisso(data, hora, descricao)
+    elif escolha == '2':
+        data = input("Informa a data que deseja consultar (dd/mm/aaaa): ")
+        hora = input(
+            "Informe a hora (hh:mm) que deseja consultar (não inclua nada para ver a agenda do dia):")
+        consult_compromisso(data, hora)
 
-#concatenando os dados em uma funcao
-        incluir_compromisso(data, hora, descricao)
-    
-    if escolha == 5:
+    elif escolha == '3':
+        data = input(
+            "Informe a data do compromisso que deseja alterar (dd/mm/aaaa):")
+        hora = input(
+            "Informe a hora do compromisso que deseja alterar (hh:mm): ")
+        descricao = input("Informe a descrição do compromisso: ")
+        alt_compromisso(data, hora, descricao)
+    elif escolha == '5':
+        print(agenda)
+
+    elif escolha == '6':
         break
+
     else:
-        print("Opção inválida.")
+        print("\nOpção inválida ou incorreta.")
